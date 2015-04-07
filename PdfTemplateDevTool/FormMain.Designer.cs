2e84583a7c9a6658b2c4c1c1c439bf1f24186715
twotymz <joshua.p.may@gmail.com>
2015-04-07 00:51:39 -0400

    Improvements.

---
+++ PdfTemplateDevTool/FormMain.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace PdfTemplateDevTool
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.axAcroPDFViewer = new AxAcroPDFLib.AxAcroPDF();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.richTextBoxExtractedText = new System.Windows.Forms.RichTextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.richTextBoxTemplate = new System.Windows.Forms.RichTextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.richTextBoxResults = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxPath = new System.Windows.Forms.TextBox();
+            this.openTemplateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.buttonRunTemplate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axAcroPDFViewer)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // axAcroPDFViewer
+            // 
+            this.axAcroPDFViewer.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.axAcroPDFViewer.Enabled = true;
+            this.axAcroPDFViewer.Location = new System.Drawing.Point(12, 27);
+            this.axAcroPDFViewer.Name = "axAcroPDFViewer";
+            this.axAcroPDFViewer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axAcroPDFViewer.OcxState")));
+            this.axAcroPDFViewer.Size = new System.Drawing.Size(511, 674);
+            this.axAcroPDFViewer.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1320, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.openTemplateToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(165, 22);
+            this.openToolStripMenuItem.Text = "Open PDF...";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.richTextBoxExtractedText);
+            this.groupBox1.Location = new System.Drawing.Point(530, 54);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(778, 209);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Extracted Text";
+            // 
+            // richTextBoxExtractedText
+            // 
+            this.richTextBoxExtractedText.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBoxExtractedText.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBoxExtractedText.Location = new System.Drawing.Point(7, 19);
+            this.richTextBoxExtractedText.Name = "richTextBoxExtractedText";
+            this.richTextBoxExtractedText.ReadOnly = true;
+            this.richTextBoxExtractedText.Size = new System.Drawing.Size(765, 184);
+            this.richTextBoxExtractedText.TabIndex = 0;
+            this.richTextBoxExtractedText.Text = "";
+            this.richTextBoxExtractedText.WordWrap = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.buttonRunTemplate);
+            this.groupBox2.Controls.Add(this.richTextBoxTemplate);
+            this.groupBox2.Location = new System.Drawing.Point(530, 269);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(778, 230);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Template";
+            // 
+            // richTextBoxTemplate
+            // 
+            this.richTextBoxTemplate.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBoxTemplate.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBoxTemplate.Location = new System.Drawing.Point(7, 20);
+            this.richTextBoxTemplate.Name = "richTextBoxTemplate";
+            this.richTextBoxTemplate.Size = new System.Drawing.Size(765, 174);
+            this.richTextBoxTemplate.TabIndex = 0;
+            this.richTextBoxTemplate.Text = "";
+            this.richTextBoxTemplate.WordWrap = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox3.Controls.Add(this.richTextBoxResults);
+            this.groupBox3.Location = new System.Drawing.Point(530, 505);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(778, 196);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Results";
+            // 
+            // richTextBoxResults
+            // 
+            this.richTextBoxResults.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBoxResults.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBoxResults.Location = new System.Drawing.Point(7, 20);
+            this.richTextBoxResults.Name = "richTextBoxResults";
+            this.richTextBoxResults.ReadOnly = true;
+            this.richTextBoxResults.Size = new System.Drawing.Size(765, 170);
+            this.richTextBoxResults.TabIndex = 0;
+            this.richTextBoxResults.Text = "";
+            this.richTextBoxResults.WordWrap = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(531, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Path";
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.Location = new System.Drawing.Point(561, 28);
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.ReadOnly = true;
+            this.textBoxPath.Size = new System.Drawing.Size(747, 20);
+            this.textBoxPath.TabIndex = 7;
+            // 
+            // openTemplateToolStripMenuItem
+            // 
+            this.openTemplateToolStripMenuItem.Name = "openTemplateToolStripMenuItem";
+            this.openTemplateToolStripMenuItem.Size = new System.Drawing.Size(165, 22);
+            this.openTemplateToolStripMenuItem.Text = "Open Template...";
+            // 
+            // buttonRunTemplate
+            // 
+            this.buttonRunTemplate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonRunTemplate.Location = new System.Drawing.Point(697, 200);
+            this.buttonRunTemplate.Name = "buttonRunTemplate";
+            this.buttonRunTemplate.Size = new System.Drawing.Size(75, 23);
+            this.buttonRunTemplate.TabIndex = 1;
+            this.buttonRunTemplate.Text = "Run Template";
+            this.buttonRunTemplate.UseVisualStyleBackColor = true;
+            this.buttonRunTemplate.Click += new System.EventHandler(this.buttonRunTemplate_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1320, 713);
+            this.Controls.Add(this.textBoxPath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.axAcroPDFViewer);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FormMain";
+            this.Text = "PDF Template Tool";
+            ((System.ComponentModel.ISupportInitialize)(this.axAcroPDFViewer)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxAcroPDFLib.AxAcroPDF axAcroPDFViewer;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox richTextBoxExtractedText;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RichTextBox richTextBoxTemplate;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RichTextBox richTextBoxResults;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxPath;
+        private System.Windows.Forms.ToolStripMenuItem openTemplateToolStripMenuItem;
+        private System.Windows.Forms.Button buttonRunTemplate;
+    }
+}
+
---
+++ PdfTemplateDevTool/FormMain.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.IO;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using PdfTemplateLib;
+
+namespace PdfTemplateDevTool
+{
+    public partial class FormMain : Form
+    {
+        string[] pdfLines;
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+
+            fileDialog.Filter = "PDF files (*.pdf)|*.pdf";
+            
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                axAcroPDFViewer.LoadFile(fileDialog.FileName);
+
+                extractText(fileDialog.FileName);
+                richTextBoxTemplate.Text = "";
+                richTextBoxResults.Text = "";
+
+                textBoxPath.Text = fileDialog.FileName;
+
+                string templatePath = Path.GetDirectoryName(fileDialog.FileName) + "\\template.txt";
+                if (File.Exists(templatePath))
+                {
+                    if (MessageBox.Show("Open template.txt from the same directory?", "Open Template", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        loadTemplate (templatePath);
+                    }
+                }                
+            }
+        }
+
+        private void extractText (string pdfPath)
+        {
+            richTextBoxExtractedText.Text = "";
+
+            using (PdfReader reader = new PdfReader(pdfPath))
+            {
+                for (int i = 1; i <= reader.NumberOfPages; i++)
+                {
+                    if (i > 1)
+                    {
+                        break;  // Just stop after the first page.
+                        richTextBoxExtractedText.AppendText("------------------------------------------------------------------\r\n");
+                        richTextBoxExtractedText.AppendText(string.Format("Page {0}\r\n\r\n", i));
+                    }
+
+                    string text = PdfTextExtractor.GetTextFromPage(reader, i);
+                    pdfLines = text.Split('\n');
+
+                    int k = 0;
+                    foreach (string line in pdfLines)
+                    {
+                        richTextBoxExtractedText.AppendText(string.Format("{0,4:G}\t{1}\r\n", k, line));
+                        ++k;
+                    }
+                }
+            }
+        }
+
+        private void loadTemplate(string templatePath)
+        {
+            richTextBoxTemplate.Text = "";
+
+            try
+            {
+                string text = File.ReadAllText(templatePath);
+                richTextBoxTemplate.AppendText(text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Failed reading template.", "Error");
+            }
+        }
+
+        private void processTemplate()
+        {
+            Template template = new Template();
+            template.SetText(richTextBoxTemplate.Text);
+            Result result = template.Run(textBoxPath.Text, pdfLines);
+            var json = JsonConvert.SerializeObject(result, Formatting.Indented, new JsonConverter[] { new StringEnumConverter() });
+
+            richTextBoxResults.Text = json;
+        }
+
+        private void buttonRunTemplate_Click(object sender, EventArgs e)
+        {
+            processTemplate();
+        }       
+    }
+}
